0279062923c94e702cdac9846dff63a1f63d08d1
vmunozq0811 <vmunoz0811@gmail.com>
2023-05-31 00:40:51 -0500

    modulo de nomina 70%

--- Constructoraweb/js/nomina.js
+++ Constructoraweb/js/nomina.js
@@ -63,8 +63,6 @@ window.addEventListener('resize', function () {
 	}
 })
 
-
-
 const switchMode = document.getElementById('switch-mode');
 
 switchMode.addEventListener('change', function () {
@@ -74,3 +72,81 @@ switchMode.addEventListener('change', function () {
 		document.body.classList.remove('dark');
 	}
 })
+
+const todoList = document.querySelector('.todo-list');
+const listItems = todoList.querySelectorAll('li');
+
+listItems.forEach((item) => {
+  item.addEventListener('click', function() {
+    if (this.classList.contains('not-completed')) {
+      this.classList.remove('not-completed');
+      this.classList.add('completed');
+    } else {
+      //this.classList.remove('completed');
+      //this.classList.add('not-completed');
+    }
+  });
+});
+
+window.onload = function() {
+	// Obtener el elemento del párrafo donde se mostrará la fecha
+	var fechaElement = document.getElementById("fecha");
+
+	// Obtener la fecha actual
+	var fechaActual = new Date();
+
+	// Obtener los componentes de la fecha (día, mes, año)
+	var dia = fechaActual.getDate();
+	var mes = fechaActual.getMonth() + 1; // Se suma 1 ya que los meses en JavaScript van de 0 a 11
+	var año = fechaActual.getFullYear();
+
+	// Mostrar la fecha actual en el elemento del párrafo
+	fechaElement.innerHTML = dia + "/" + mes + "/" + año;
+};
+
+const empleadoLink = document.getElementById('empleado'),
+  		inicioLink = document.getElementById('inicio'),
+		nominaLink = document.getElementById('nomina'),
+		novedadesLink = document.getElementById('novedades'),
+		seguridadLink = document.getElementById('seguridad'),
+		contratosLink = document.getElementById('contratos'),
+		dashboardContainer = document.querySelector('.dashboard-container'),
+		empleadosContainer = document.querySelector('.empleados-container'),
+		nominaContainer = document.querySelector('.nomina-container'),
+		novedadesContainer = document.querySelector('.novedades-container');
+
+inicioLink.addEventListener('click', function(event) {
+	event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+	dashboardContainer.style.display = 'block';
+	empleadosContainer.style.display = 'none';
+	nominaContainer.style.display = 'none';
+	novedadesContainer.style.display = 'none';
+});
+
+empleadoLink.addEventListener('click', function(event) {
+	event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+	dashboardContainer.style.display = 'none';
+	empleadosContainer.style.display = 'block';
+	nominaContainer.style.display = 'none';
+	novedadesContainer.style.display = 'none';
+});
+
+nominaLink.addEventListener('click', function(event) {
+	event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+	dashboardContainer.style.display = 'none';
+	empleadosContainer.style.display = 'none';
+	nominaContainer.style.display = 'block';
+	novedadesContainer.style.display = 'none';
+});
+
+novedadesLink.addEventListener('click', function(event) {
+	event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+	dashboardContainer.style.display = 'none';
+	empleadosContainer.style.display = 'none';
+	nominaContainer.style.display = 'none';
+	novedadesContainer.style.display = 'block';
+});
+
+
+
+
